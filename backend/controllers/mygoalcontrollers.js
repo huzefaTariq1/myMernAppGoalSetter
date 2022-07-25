cee40e643470d0e8aa8f24947c5e5a8804090bc5
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-07-25 22:51:46 +0500

    handilng input validation with Joi

--- backend/controllers/mygoalcontrollers.js
+++ backend/controllers/mygoalcontrollers.js
@@ -1,23 +1,39 @@
-const getGoals=(req,res)=>{
- res.send("getting goalss")
+const Joi = require("joi")
+
+
+const getGoals = (req, res) => {
+    res.send("getting goalss")
 }
 
 
-const postGoal=(req,res)=>{
+const postGoal = (req, res) => {
+
+    const joiSchema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, joiSchema)
+
+
+    if (result.error) {
+        //400 bad request
+        res.status(400).send(result.error.details[0].message)
+    }
+
     res.send("posting goals")
 }
 
-const putGoal=(req,res)=>{
+const putGoal = (req, res) => {
     res.send(`update goal ${req.params.id}`)
 }
 
-const deleteGoal=(req,res)=>{
+const deleteGoal = (req, res) => {
     res.send(`delete goal ${req.params.id}`)
 }
 
-module.exports={
-getGoals,
-postGoal,
-putGoal,
-deleteGoal
+module.exports = {
+    getGoals,
+    postGoal,
+    putGoal,
+    deleteGoal
 }
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const dotenv=require("dotenv").config()
 const port =process.env.PORT || 5000
 console.log(port)
 const app=express()
-
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use('/api/goals',require('./routes/goalRoutes'))
+
+
 
 
 app.listen(port,()=>console.log( `app is running on port ${port}`))
